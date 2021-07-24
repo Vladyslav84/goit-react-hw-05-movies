ed5de250a728a32c79774af4aafdf2361e029be9
Vladyslav84 <72793544+Vladyslav84@users.noreply.github.com>
2021-07-24 11:13:21 +0300

    Починаю кнопку гоубєк.

--- src/components/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink, Route, useParams, useRouteMatch, Switch } from 'react-router-dom';
+import { NavLink, Route, useParams, useRouteMatch, Switch, useHistory, useLocation } from 'react-router-dom';
 import s from './MovieDetailsPage.module.css';
 import Casts from '../Cast/Cast';
 import Reviews from '../Reviews/Reviews';
@@ -7,21 +7,28 @@ import noPoster from '../../imagesDef/noposter.png'
 import * as api from '../../api/api';
 
 const MovieDetailsPage = () => {
+    const location = useLocation();
+    const history = useHistory();
     const { url, path } = useRouteMatch();
     const { movieId } = useParams();
     const imgBasePath = 'https://image.tmdb.org/t/p/w500'
     const [movieIdObj, setmovieIdObj] = useState();
 
+    // const handleGoBack = () => {
+    //     history.push('/');
+    // }
+    console.log('location :>> ', location);
+    console.log('history :>> ', history);
     useEffect(() => {
         api.fetchMoviesId(movieId).then(res => setmovieIdObj(res))
-// eslint-disable-next-line 
+        // eslint-disable-next-line 
     }, [])
 
     return (<>
         {movieIdObj && <>
-          <button type="button">Go back</button>
+            <button type="button" >Go back</button>
             <article>
-                <img src={imgBasePath + movieIdObj.poster_path !== "https://image.tmdb.org/t/p/w500null"?imgBasePath + movieIdObj.poster_path: noPoster } alt={movieIdObj.original_title} width="150" ></img>
+                <img src={imgBasePath + movieIdObj.poster_path !== "https://image.tmdb.org/t/p/w500null" ? imgBasePath + movieIdObj.poster_path : noPoster} alt={movieIdObj.original_title} width="150" ></img>
                 <ul>
                     <li><h2>{movieIdObj.original_title}</h2>
                     </li>
--- src/components/MoviesPage/MoviesPage.jsx
+++ src/components/MoviesPage/MoviesPage.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import s from './MoviesPage.module.css'
 import * as api from '../../api/api';
 
 const MoviesPage = () => {
-
+    const history = useHistory();
     const [submitValue, setSubmitValue] = useState('')
     const [film, setFilm] = useState([])
 
@@ -13,13 +13,14 @@ const MoviesPage = () => {
         {
             api.fetchMoviesSearch(submitValue).then(res => setFilm(res))
         }
-            setSubmitValue('');
-// eslint-disable-next-line 
+
+        // eslint-disable-next-line 
     }, [submitValue])
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
         setSubmitValue(evt.target.elements.inputName.value)
+        history.push(`/Movies/?query=${ evt.target.elements.inputName.value }`)
         evt.target.reset();
     }
 
@@ -30,17 +31,17 @@ const MoviesPage = () => {
                 <input type="text"
                     autoComplete="off"
                     autoFocus
-                    name = "inputName"
+                    name="inputName"
                 />
                 <button type='submit'> Search</button>
             </form>
 
-           <ul>
-            {film && film.map(film =>
-                <li key={film.id} >
-                    <Link to={`/Movies/${ film.id }`} className={s.navLinks}>{film.title}<span>Popularity</span>{film.vote_average}
-                    </Link>
-                </li>)}
+            <ul>
+                {film && film.map(film =>
+                    <li key={film.id} >
+                        <Link to={`/Movies/${ film.id }`} className={s.navLinks}>{film.title}<span>Popularity</span>{film.vote_average}
+                        </Link>
+                    </li>)}
             </ul>
         </>
 
